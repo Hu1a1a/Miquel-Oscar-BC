550bc72d5e017167b29502ac0e619e1edcb34169
Hulala <yang.ye.1@hotmail.com>
2024-06-13 12:29:37 +0200

    Update

--- 3.js
+++ 3.js
@@ -1,27 +1,27 @@
 const Categoria = [
-    { Name: "+ TESTARUDO", Icon: "assets/icon/1.png", },
-    { Name: "+ ENÉRGICO", Icon: "assets/icon/2.png", },
-    { Name: "+ DIVERTIDO", Icon: "assets/icon/3.png", },
-    { Name: "+ ESTRATEGA", Icon: "assets/icon/3.png", },
-    { Name: "+ SIMPATICO", Icon: "assets/icon/3.png", },
-    { Name: "+ APLICADO", Icon: "assets/icon/3.png", },
-    { Name: "+ TRANQUILO", Icon: "assets/icon/3.png", },
-    { Name: "+ OPTIMISTA", Icon: "assets/icon/3.png", },
-    { Name: "+ CREATIVO", Icon: "assets/icon/3.png", },
-    { Name: "+ EMPATICO", Icon: "assets/icon/3.png", },
-    { Name: "+ GUAPO/A", Icon: "assets/icon/3.png", },
-    { Name: "+ ASTUTO", Icon: "assets/icon/3.png", },
-    { Name: "+ COLABORATIVO", Icon: "assets/icon/3.png", },
-    { Name: "+ GENUINO", Icon: "assets/icon/3.png", },
-    { Name: "+ INDISCRETO", Icon: "assets/icon/3.png", },
-    { Name: "+ PUNTUAL", Icon: "assets/icon/3.png", },
-    { Name: "+ EXIGENTE", Icon: "assets/icon/3.png", },
-    { Name: "+ TACAÑO", Icon: "assets/icon/3.png", },
-    { Name: "+ TIMIDO", Icon: "assets/icon/3.png", },
-    { Name: "+ INDEPENDIENTE", Icon: "assets/icon/3.png", },
-    { Name: "+ LÍDER", Icon: "assets/icon/3.png", },
-    { Name: "+ DIPLOMATICO", Icon: "assets/icon/3.png", },
-    { Name: "+ PRUDENTE", Icon: "assets/icon/3.png", },
+    { Name: "+ TESTARUDO", Icon: "assets/ICONS/testarudo.png", Descripcion: "Persona que se mantiene firme en sus opiniones y decisiones, sin ceder fácilmente.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ ENÉRGICO", Icon: "assets/ICONS/energ.png", Descripcion: "Persona llena de vitalidad, con gran vigor y dinamismo en todas sus acciones.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ DIVERTIDO", Icon: "assets/ICONS/divert.png", Descripcion: "Persona que irradia alegría y humor, siempre capaz de hacer reír a los demás.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ ESTRATEGA", Icon: "assets/ICONS/estrat.png", Descripcion: "Persona que planifica y ejecuta acciones con habilidad y visión a largo plazo.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ SIMPATICO", Icon: "assets/ICONS/simp.png", Descripcion: "Persona amigable y agradable que genera simpatía y buenas relaciones con los demás.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ APLICADO", Icon: "assets/ICONS/apli.png", Descripcion: "Individuo que demuestra dedicación y esfuerzo constante en sus tareas y responsabilidades.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ TRANQUILO", Icon: "assets/ICONS/tranq.png", Descripcion: "Persona calmada y serena, que mantiene la paz interior en situaciones de estrés.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ OPTIMISTA", Icon: "assets/ICONS/opt.png", Descripcion: "Alguien que tiene una visión positiva y esperanzadora sobre el futuro y las situaciones.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ CREATIVO", Icon: "assets/ICONS/creat.png", Descripcion: "Individuo que genera ideas originales e innovadoras, aportando soluciones nuevas.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ EMPATICO", Icon: "assets/ICONS/empa.png", Descripcion: "Alguien que comprende y comparte los sentimientos de los demás, mostrando comprensión.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ GUAPO/A", Icon: "assets/ICONS/guap.png", Descripcion: "Alguien que destaca por su atractivo físico y apariencia estética agradable.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ ASTUTO", Icon: "assets/ICONS/ast.png", Descripcion: "Persona ingeniosa y hábil para resolver problemas y situaciones complejas con sagacidad.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ COLABORATIVO", Icon: "assets/ICONS/colab.png", Descripcion: "Individuo que trabaja bien en equipo, aportando y compartiendo ideas y esfuerzos.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ GENUINO", Icon: "assets/ICONS/gen.png", Descripcion: "Persona auténtica y sincera, que se muestra tal como es sin fingimientos.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ INDISCRETO", Icon: "assets/ICONS/indis.png", Descripcion: "Alguien que no guarda secretos ni confidencias, hablando más de lo debido.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ PUNTUAL", Icon: "assets/ICONS/punt.png", Descripcion: "Persona que respeta los horarios y llega a tiempo a compromisos y obligaciones.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ EXIGENTE", Icon: "assets/ICONS/exig.png", Descripcion: "Individuo que demanda alta calidad y esfuerzo, tanto de sí mismo como de los demás.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ TACAÑO", Icon: "assets/ICONS/tac.png", Descripcion: "Persona que evita gastar dinero y recursos, siendo excesivamente ahorrativa.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ TIMIDO", Icon: "assets/ICONS/tim.png", Descripcion: "Persona reservada y con dificultades para interactuar socialmente por inseguridad.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ INDEPENDIENTE", Icon: "assets/ICONS/indep.png", Descripcion: "Individuo que actúa y toma decisiones por sí mismo, sin depender de otros.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ LÍDER", Icon: "assets/ICONS/lid.png", Descripcion: "Persona que guía y motiva a los demás, ejerciendo influencia y dirección.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ DIPLOMATICO", Icon: "assets/ICONS/dip.png", Descripcion: "Alguien que maneja situaciones con tacto y habilidad, evitando conflictos y ofensas.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ PRUDENTE", Icon: "assets/ICONS/prud.png", Descripcion: "Persona que actúa con cautela y consideración, evitando riesgos innecesarios.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
 ]
 
 let i = -1
@@ -34,7 +34,7 @@ function next() {
         for (let j = i * d; j <= Math.min(Categoria.length - 1, i * d + d - 1); j++) {
             HTML +=
                 `<div class="col-3">
-            <img src="${Categoria[j].Icon}" alt>
+            <img src="${Categoria[j].Icon}" alt class="IconImg">
             <br>
             <img src="assets/icon/inter.png" alt>
             <br>
--- 6.js
+++ 6.js
@@ -25,29 +25,29 @@ const Nominados = [
 ]
 
 const Categoria = [
-    { Name: "+ TESTARUDO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ ENÉRGICO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ DIVERTIDO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ ESTRATEGA", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ SIMPATICO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ APLICADO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ TRANQUILO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ OPTIMISTA", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ CREATIVO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ EMPATICO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ GUAPO/A", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ ASTUTO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ COLABORATIVO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ GENUINO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ INDISCRETO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ PUNTUAL", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ EXIGENTE", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ TACAÑO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ TIMIDO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ INDEPENDIENTE", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ LÍDER", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ DIPLOMATICO", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
-    { Name: "+ PRUDENTE", Dep: "AL MAS GUAP@ DE BC?", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ TESTARUDO", Icon: "assets/ICONS/testarudo.png", Descripcion: "Persona que se mantiene firme en sus opiniones y decisiones, sin ceder fácilmente.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ ENÉRGICO", Icon: "assets/ICONS/energ.png", Descripcion: "Persona llena de vitalidad, con gran vigor y dinamismo en todas sus acciones.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ DIVERTIDO", Icon: "assets/ICONS/divert.png", Descripcion: "Persona que irradia alegría y humor, siempre capaz de hacer reír a los demás.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ ESTRATEGA", Icon: "assets/ICONS/estrat.png", Descripcion: "Persona que planifica y ejecuta acciones con habilidad y visión a largo plazo.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ SIMPATICO", Icon: "assets/ICONS/simp.png", Descripcion: "Persona amigable y agradable que genera simpatía y buenas relaciones con los demás.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ APLICADO", Icon: "assets/ICONS/apli.png", Descripcion: "Individuo que demuestra dedicación y esfuerzo constante en sus tareas y responsabilidades.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ TRANQUILO", Icon: "assets/ICONS/tranq.png", Descripcion: "Persona calmada y serena, que mantiene la paz interior en situaciones de estrés.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ OPTIMISTA", Icon: "assets/ICONS/opt.png", Descripcion: "Alguien que tiene una visión positiva y esperanzadora sobre el futuro y las situaciones.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ CREATIVO", Icon: "assets/ICONS/creat.png", Descripcion: "Individuo que genera ideas originales e innovadoras, aportando soluciones nuevas.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ EMPATICO", Icon: "assets/ICONS/empa.png", Descripcion: "Alguien que comprende y comparte los sentimientos de los demás, mostrando comprensión.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ GUAPO/A", Icon: "assets/ICONS/guap.png", Descripcion: "Alguien que destaca por su atractivo físico y apariencia estética agradable.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ ASTUTO", Icon: "assets/ICONS/ast.png", Descripcion: "Persona ingeniosa y hábil para resolver problemas y situaciones complejas con sagacidad.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ COLABORATIVO", Icon: "assets/ICONS/colab.png", Descripcion: "Individuo que trabaja bien en equipo, aportando y compartiendo ideas y esfuerzos.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ GENUINO", Icon: "assets/ICONS/gen.png", Descripcion: "Persona auténtica y sincera, que se muestra tal como es sin fingimientos.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ INDISCRETO", Icon: "assets/ICONS/indis.png", Descripcion: "Alguien que no guarda secretos ni confidencias, hablando más de lo debido.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ PUNTUAL", Icon: "assets/ICONS/punt.png", Descripcion: "Persona que respeta los horarios y llega a tiempo a compromisos y obligaciones.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ EXIGENTE", Icon: "assets/ICONS/exig.png", Descripcion: "Individuo que demanda alta calidad y esfuerzo, tanto de sí mismo como de los demás.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ TACAÑO", Icon: "assets/ICONS/tac.png", Descripcion: "Persona que evita gastar dinero y recursos, siendo excesivamente ahorrativa.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ TIMIDO", Icon: "assets/ICONS/tim.png", Descripcion: "Persona reservada y con dificultades para interactuar socialmente por inseguridad.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ INDEPENDIENTE", Icon: "assets/ICONS/indep.png", Descripcion: "Individuo que actúa y toma decisiones por sí mismo, sin depender de otros.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ LÍDER", Icon: "assets/ICONS/lid.png", Descripcion: "Persona que guía y motiva a los demás, ejerciendo influencia y dirección.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ DIPLOMATICO", Icon: "assets/ICONS/dip.png", Descripcion: "Alguien que maneja situaciones con tacto y habilidad, evitando conflictos y ofensas.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
+    { Name: "+ PRUDENTE", Icon: "assets/ICONS/prud.png", Descripcion: "Persona que actúa con cautela y consideración, evitando riesgos innecesarios.", first: "LBS-Lara", second: "KRM-Kevin", third: "MGL-Manolo", music: "assets/media/18 Survivor - Eye of the Tiger.mp3" },
 ]
 
 let i = -1
@@ -67,10 +67,7 @@ function next() {
             let HTML = ""
             HTML +=
                 `
-            <h1 class="col-12 title"> ${Categoria[i].Dep} </h1>
-            <br>
-            <h1 class="col-12 subtitle" style="margin-bottom: 100px"> ${Categoria[i].Name} </h1>
-            <br>
+            <h1 class="col-12 title" style="margin-bottom:100px"> ${Categoria[i].Name} </h1>
             <div class="col-4">
             <div class="nominado">
             <img src="assets/nominado/marco.png" alt class="marco2">
@@ -94,7 +91,10 @@ function next() {
             <br>
             <span class="subtitle"> ${Categoria[i][premio[2]]} </span>
             </div>
-            </div>`
+            </div>
+            <br>
+            <h1 class="col-12 subtitle" style="margin: 100px 0"> ${Categoria[i].Descripcion} </h1>
+`
             document.getElementById("nominados").innerHTML = HTML
             xxnext = false
             loading = false
