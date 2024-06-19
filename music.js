0eba21fbe14b7620dd35d8123b77456b21682ad1
Hulala <yang.ye.1@hotmail.com>
2024-06-19 13:09:27 +0200

    Update music

--- music.js
+++ music.js
@@ -3,4 +3,5 @@ function Gameloop() {
     document.getElementById("music").play();
     if (!document.getElementById("music").paused) document.getElementById("music").style.visibility = "unset";
 }
-window.onload = setInterval(Gameloop, 1000 / 10); //10fps
+
+window.onload = setInterval(Gameloop, 1000 / 10); 
